b3d50d30f2049e1f17648a17a7d7f6730ec483fa
roberta <roberta.giordano950@gmail.com>
2023-09-01 14:42:28 +0200

    Updated with a searchbar and a list of cars retrieved by the gist. Added a detail page that contains the details of each car.

---
+++ js/app.js
@@ -0,0 +1,50 @@
+const carList = document.getElementById("carList");
+const searchBar = document.getElementById("searchBar");
+let carModels = [];
+
+// This is a async function that loads a list of objects of an array from a remote json file, a bunch of cars. With the 'fetch' im sending a get request and waiting a response with 'await'. The 'response' contains the response from the server. Im parsing data from the json and assign it to carModels. After all this is done im passing the data from carModels inside the dispayCarsInfo in order to be displayed on the webpage. There is also a try-catch for eventual errors.
+let loadCarsList = async () => {
+  try {
+    const response = await fetch(
+      "https://gist.githubusercontent.com/joaofs/6a4eb62499572a29485ac5924a0c9e64/raw/97ac2191e65fb6d84f6f336dc8867efbc97410a3/cars.json"
+    );
+    carModels = await response.json();
+    displayCarsInfo(carModels);
+  } catch (error) {
+    console.error(error);
+  }
+};
+// This function uses .map to iterate the cars array and for each car object it generates a <li> that contains the car info like the make, model and price. All the info is concatenated with the .join into a single string. Then i save all of it to a variable called htmlString and i set the ul of the index.html(carList) to it in order to display the info on the page.
+const displayCarsInfo = (cars) => {
+  const htmlString = cars
+    .map((car, index) => {
+      return `
+                <li class="cars" onclick="openDetailPage(${index})">
+                    <img src="./assets/images/Tesla_Model_S.avif" />
+                    <h2>${car.make} ${car.model}</h2>
+                    <p>$${car.price}</p>
+                </li>`;
+    })
+    .join("");
+  carList.innerHTML = htmlString;
+};
+loadCarsList();
+
+// This function opens a new page in a new tab, it receives a index parameter in order to open the correct object with its info.
+function openDetailPage(index) {
+  const url = `detail-page.html?index=${index}`;
+  window.open(url, "_blank");
+}
+
+// This is a searchbar that has an eventlistener attached to it that gets triggered by the user. The event listens to a 'keyup', it convers the text into lowercase to make it case-insensitive. I filter through the carModels array and use the .filter to create a new array that contains only the cars objects that match what the user typed.
+searchBar.addEventListener("keyup", (key) => {
+  const searchString = key.target.value.toLowerCase();
+
+  const filteredCars = carModels.filter((cars) => {
+    return (
+      cars.make.toLowerCase().includes(searchString) ||
+      cars.model.toLowerCase().includes(searchString)
+    );
+  });
+  displayCarsInfo(filteredCars);
+});
---
+++ js/detail-page.js
@@ -0,0 +1,37 @@
+const carDetail = document.getElementById("carDetail");
+
+// Here i get the value associated with the 'index' from urlParams.
+const urlParams = new URLSearchParams(window.location.search);
+const carIndex = urlParams.get("index"); 
+
+// This is another async function that essentialy loads the same list of cars from the list of objects from the remote json file and display the details of a specific car. The difference is that here we check if carmodels is not null and if the carIndex exsist in the carModel array. I store the car object in a car variable in the if statement with its index and pass it as an arguemnt to the displayCarDetail function that will display it in the detail-page.html
+let loadCarDetail = async () => {
+  try {
+    const response = await fetch(
+      "https://gist.githubusercontent.com/joaofs/6a4eb62499572a29485ac5924a0c9e64/raw/97ac2191e65fb6d84f6f336dc8867efbc97410a3/cars.json"
+    );
+    const carModels = await response.json();
+
+    if (carModels && carModels[carIndex]) {
+      const car = carModels[carIndex];
+      displayCarDetail(car);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+loadCarDetail();
+
+// This is a function that takes 'car' as paramter and generates a string that gets set to the carDetail element in from the detail-page.html
+const displayCarDetail = (car) => {
+  const htmlString = `
+        <h2>${car.make} ${car.model}</h2>
+        <img src="./assets/images/Tesla_Model_S.avif"/>
+        <p>${car.description}</p>
+        <p>${car.features}</p>
+        <p>Miles: ${car.mileage}</p>
+        <p>Price: $${car.price}</p>
+        
+    `;
+  carDetail.innerHTML = htmlString;
+};
